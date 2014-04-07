fb9a7713bc435b6d8aeee5b337f41bc8aa960fa1
Martxel Lasa <martxel@gmail.com>
2014-04-07 15:59:43 +0200

    Simplify install and uninstall scripts.

--- uninstall.sh
+++ uninstall.sh
@@ -4,9 +4,13 @@
 files="zprezto vim ctags dir_colors gemrc gitconfig gitignore tmux.conf \
   vimrc vimrc.bundles zlogin zlogout zpreztorc zprofile zshenv zshrc"
 
-# link dotfiles
+# remove symbolic links
 for file in $files; do
-  ln -sv $PWD/$file $HOME/.$file
+  if [[ -h $HOME/.$file ]]; then
+    rm -frv $HOME/.$file
+  else
+    echo $HOME/.$file is not a symbolic link or doesn't exist
+  fi
 done
 
 echo Done!
