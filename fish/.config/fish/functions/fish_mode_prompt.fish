ade340f1cd0dba2aa039107d3f28e46831905c01
Martxel Lasa <martxel@gmail.com>
2015-08-28 10:55:56 +0200

    Remove colors from vi mode indicator.

--- fish/.config/fish/functions/fish_mode_prompt.fish
+++ fish/.config/fish/functions/fish_mode_prompt.fish
@@ -4,13 +4,10 @@ function fish_mode_prompt --description "Displays the current mode"
   if set -q __fish_vi_mode
     switch $fish_bind_mode
       case default
-        set_color red
         echo '[N]'
       case insert
-        set_color green
         echo '[I]'
       case visual
-        set_color magenta
         echo '[V]'
     end
     set_color normal
